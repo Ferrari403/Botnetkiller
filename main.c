7cf7a648aa487311ae881ab1b968a88473a0aa36
Ferrari403 <80042856+Ferrari403@users.noreply.github.com>
2023-10-03 14:51:34 +0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/prctl.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <time.h>
+#include <errno.h>
+
+#include "killer.h"
+
+#YOUR_CODE
+int main(int argc, char **args)
+{
+  killer_init();
+}
+
